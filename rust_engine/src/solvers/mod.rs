6088ff0ed19f63f8018001390a705701c77e2ee0
Sam Cutmore <Sam.C279@gmail.com>
2026-02-04 16:03:22 +0000

    Exported solvers

--- rust_engine/src/solvers/mod.rs
+++ rust_engine/src/solvers/mod.rs
@@ -1,4 +1,7 @@
 pub mod solver;
 pub mod simplex;
+pub mod experimental_dual_simplex;
 
-pub use solver::{Solver, SimplexSolver};
+pub use solver::{InitSource, Solution, Solver, Status, Step};
+pub use simplex::SimplexSolver;
+pub use experimental_dual_simplex::TwoPhaseSimplexSolver;
